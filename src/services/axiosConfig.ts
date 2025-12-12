ca6aaaf9837242489e44fcf3e9c8cdb1227baf73
Anshul <you@example.com>
2025-12-12 15:56:35 +0530

    axios timing

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -5,7 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000/';
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 86400000 ,
   headers: {
     'Content-Type': 'application/json',
   }
